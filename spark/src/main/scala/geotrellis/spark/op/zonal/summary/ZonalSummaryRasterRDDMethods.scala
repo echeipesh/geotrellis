aabb6efc0b5d079f0aaf629a2c8369102ea505d3
Eugene Cheipesh <echeipesh@gmail.com>
2014-12-03 17:30:40 -0500

    added zonal summary by key

--- spark/src/main/scala/geotrellis/spark/op/zonal/summary/ZonalSummaryRasterRDDMethods.scala
+++ spark/src/main/scala/geotrellis/spark/op/zonal/summary/ZonalSummaryRasterRDDMethods.scala
@@ -3,26 +3,93 @@ package geotrellis.spark.op.zonal.summary
 import geotrellis.raster.op.zonal.summary._
 import geotrellis.raster.stats._
 import geotrellis.raster._
-
 import geotrellis.spark._
-
 import geotrellis.vector._
 
+import org.apache.spark.rdd._
+import org.apache.spark.SparkContext._
+import org.joda.time._
 import reflect.ClassTag
 
 trait ZonalSummaryRasterRDDMethods[K] extends RasterRDDMethods[K] {
 
   protected implicit val _sc: SpatialComponent[K]
 
-  private def zonalSummary[T: ClassTag](
+  def zonalSummary[T: ClassTag](
     polygon: Polygon,
     zeroValue: T,
     handleTileIntersection: TileIntersection => T,
-    combineOp: (T, T) => T): T = {
+    combineOp: (T, T) => T
+  ): T = {
     val sc = rasterRDD.sparkContext
     val bcPolygon = sc.broadcast(polygon)
     val bcMetaData = sc.broadcast(rasterRDD.metaData)
 
+    // something that folds shit our RDD is made of into a value
+    def seqOp(v: T, t: (K, Tile)): T = {
+      val p = bcPolygon.value
+      val extent = bcMetaData.value.mapTransform(t.id)
+      val tile = t.tile
+
+      val rs =
+        if (p.contains(extent)) handleTileIntersection(FullTileIntersection(tile))
+        else {
+          val polys = p.intersection(extent) match {
+            case PolygonResult(intersectionPoly) => Seq(intersectionPoly)
+            case MultiPolygonResult(mp) => mp.polygons.toSeq
+            case _ => Seq()
+          }
+
+          polys.map(PartialTileIntersection(tile, extent, _))
+            .map(handleTileIntersection).fold(zeroValue)(combineOp)
+        }
+
+      combineOp(v, rs)         // if the tile does not contain an intersection, rs will be a zeroValue
+    }
+
+    rasterRDD.aggregate(zeroValue)(seqOp, combineOp)
+  }
+
+  def zonalSummary[T: ClassTag](
+    polygon: Polygon,
+    zeroValue: T,
+    handler: TileIntersectionHandler[T, T]
+  ): T = {
+    zonalSummary(
+      polygon,
+      zeroValue,
+      handler,
+      (a: T, b: T) => handler.combineResults(Seq(a, b))
+    )
+  }
+
+  def zonalSummaryByKey[T: ClassTag, L: ClassTag](
+    polygon: Polygon,
+    zeroValue: T,
+    handler: TileIntersectionHandler[T, T],
+    fKey: K => L
+  ): RDD[(L, T)] = {
+    zonalSummaryByKey(
+      polygon,
+      zeroValue,
+      handler,
+      (a: T, b: T) => handler.combineResults(Seq(a, b)),
+      fKey
+    )
+  }
+
+  def zonalSummaryByKey[T: ClassTag, L: ClassTag](
+    polygon: Polygon,
+    zeroValue: T,
+    handleTileIntersection: TileIntersection => T,
+    combineOp: (T, T) => T,
+    fKey: K => L
+  ): RDD[(L, T)] = {
+
+    val sc = rasterRDD.sparkContext
+    val bcPolygon = sc.broadcast(polygon)
+    val bcMetaData = sc.broadcast(rasterRDD.metaData)
+    
     def seqOp(v: T, t: (K, Tile)): T = {
       val p = bcPolygon.value
       val extent = bcMetaData.value.mapTransform(t.id)
@@ -43,20 +110,10 @@ trait ZonalSummaryRasterRDDMethods[K] extends RasterRDDMethods[K] {
 
       combineOp(v, rs)
     }
-
-    rasterRDD.aggregate(zeroValue)(seqOp, combineOp)
-  }
-
-  private def zonalSummary[T: ClassTag](
-    polygon: Polygon,
-    zeroValue: T,
-    handler: TileIntersectionHandler[T, T]): T = {
-    zonalSummary(
-      polygon,
-      zeroValue,
-      handler,
-      (a: T, b: T) => handler.combineResults(Seq(a, b))
-    )
+    
+    rasterRDD
+      .map { case (key, tile) => fKey(key) -> (key -> tile) }
+      .aggregateByKey(zeroValue)(seqOp, combineOp)
   }
 
   def zonalHistogram(polygon: Polygon): Histogram =

85885a5b6ed447b90cded37dcab92b7d34231ab9
Eugene Cheipesh <echeipesh@gmail.com>
2014-12-09 13:49:15 -0500

    testing calculate

--- spark/src/main/scala/geotrellis/climate/cmd/Calculate.scala
+++ spark/src/main/scala/geotrellis/climate/cmd/Calculate.scala
@@ -11,7 +11,7 @@ import geotrellis.spark.op.stats._
 import geotrellis.spark.utils.SparkUtils
 import geotrellis.vector.Extent
 import org.apache.accumulo.core.client.security.tokens.PasswordToken
-
+import org.apache.hadoop.fs.Path
 import org.apache.spark._
 
 class CalculateArgs extends AccumuloArgs {
@@ -26,24 +26,25 @@ object Calculate extends ArgMain[CalculateArgs] with Logging {
   def main(args: CalculateArgs): Unit = {
     implicit val sparkContext = SparkUtils.createSparkContext("Calculate")
 
-    val accumulo = AccumuloInstance(args.instance, args.zookeeper, args.user, new PasswordToken(args.password))
-    val catalog = accumulo.catalog
+    // val accumulo = AccumuloInstance(args.instance, args.zookeeper, args.user, new PasswordToken(args.password))
+    // val catalog = accumulo.catalog
+    val catalog: HadoopCatalog = HadoopCatalog(sparkContext, new Path("hdfs://localhost/catalog"))
 
     val rdd = catalog.load[SpaceTimeKey](LayerId(args.inputLayer, 2)).get
     
-    // val ret = rdd
-    //   .mapKeys { key => key.updateTemporalComponent(key.temporalKey.time.withMonthOfYear(1).withDayOfMonth(1).withHourOfDay(0)) }
-    //   .averageByKey
-    // catalog.save[SpaceTimeKey](LayerId(args.outputLayer,2), "results", ret, true).get
+    val ret = rdd
+      .mapKeys { key => key.updateTemporalComponent(key.temporalKey.time.withMonthOfYear(1).withDayOfMonth(1).withHourOfDay(0)) }
+      .averageByKey
+    catalog.save[SpaceTimeKey](LayerId(args.outputLayer,2), "results", ret, true).get
 
-    import geotrellis.spark.op.zonal.summary._
-    import geotrellis.raster.op.zonal.summary._
-    val polygon = Extent(-13193016.062816, 3088377.007329,
-                          -8453323.832114, 5722687.564266)
+//    import geotrellis.spark.op.zonal.summary._
+    // import geotrellis.raster.op.zonal.summary._
+    // val polygon = Extent(-13193016.062816, 3088377.007329,
+    //                       -8453323.832114, 5722687.564266)
     
-    val min = rdd.zonalSummaryByKey(polygon, Int.MinValue, Min, stk => stk.temporalComponent.time)
-    val max = rdd.zonalSummaryByKey(polygon, Int.MinValue, Max, stk => stk.temporalComponent.time)
-    val mean = rdd.zonalSummaryByKey(polygon, MeanResult(0.0, 0L), Mean, (a: MeanResult, b: MeanResult) => a + b, stk => stk.temporalComponent.time)
-    min zip max zip mean foreach (println)
+    // val min = rdd.zonalSummaryByKey(polygon, Int.MinValue, Min, stk => stk.temporalComponent.time)
+    // val max = rdd.zonalSummaryByKey(polygon, Int.MinValue, Max, stk => stk.temporalComponent.time)
+    // val mean = rdd.zonalSummaryByKey(polygon, MeanResult(0.0, 0L), Mean, (a: MeanResult, b: MeanResult) => a + b, stk => stk.temporalComponent.time)
+    // min zip max zip mean foreach (println)
   }
 }

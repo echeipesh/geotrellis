946ff845345a9b05cf3c4f9b8a551f58c928bfda
Eugene Cheipesh <echeipesh@gmail.com>
2014-12-10 12:00:53 -0500

    splitting out ZonalSummaryRoutes into it's own trait

--- spark/src/main/scala/geotrellis/climate/rest/CatalogService.scala
+++ spark/src/main/scala/geotrellis/climate/rest/CatalogService.scala
@@ -1,9 +1,9 @@
 package climate.rest
 
-
 import akka.actor.ActorSystem
+import climate.op._
+import com.github.nscala_time.time.Imports._
 import com.quantifind.sumac.{ ArgApp, ArgMain }
-
 import geotrellis.proj4._
 import geotrellis.raster._
 import geotrellis.raster.io.geotiff.Nodata
@@ -12,46 +12,27 @@ import geotrellis.spark._
 import geotrellis.spark.cmd.args._
 import geotrellis.spark.io.accumulo._
 import geotrellis.spark.io.hadoop._
-import org.apache.hadoop.fs.Path
 import geotrellis.spark.json._
 import geotrellis.spark.tiling._
 import geotrellis.spark.utils.SparkUtils
-import geotrellis.vector.reproject._
 import geotrellis.vector._
-import geotrellis.vector.json._
-
+import geotrellis.vector.reproject._
 import org.apache.accumulo.core.client.security.tokens.PasswordToken
-import org.joda.time.DateTime
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
-
-import spray.http.HttpHeaders._
-import spray.http.HttpMethods._
-import spray.http.{ AllOrigins, MediaTypes }
-import spray.http.{ HttpMethods, HttpMethod, HttpResponse, AllOrigins }
+import spray.http.{MediaTypes }
 import spray.httpx.SprayJsonSupport
 import spray.json._
 import spray.routing._
-import spray.routing.SimpleRoutingApp
-import climate.op._
-import com.github.nscala_time.time.Imports._
 
-trait CORSSupport { self: HttpService =>
-  val corsHeaders = List(`Access-Control-Allow-Origin`(AllOrigins),
-    `Access-Control-Allow-Methods`(GET, POST, OPTIONS, DELETE),
-    `Access-Control-Allow-Headers`("Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding, Accept-Language, Host, Referer, User-Agent"))
 
-  def cors: Directive0 = {
-    val rh = implicitly[RejectionHandler]
-    respondWithHeaders(corsHeaders) & handleRejections(rh)
-  }
-}
 class CatalogArgs extends AccumuloArgs
+
 /**
  * Catalog and TMS service for TimeRaster layers only
  * This is intended to exercise the machinery more than being a serious service.
  */
-object CatalogService extends ArgApp[CatalogArgs] with SimpleRoutingApp with SprayJsonSupport with CORSSupport {
+object CatalogService extends ArgApp[CatalogArgs] with SimpleRoutingApp with SprayJsonSupport with CorsSupport with ZonalSummaryRoutes {
   implicit val system = ActorSystem("spray-system")
   implicit val sparkContext = SparkUtils.createSparkContext("Catalog")
 
@@ -156,9 +137,6 @@ object CatalogService extends ArgApp[CatalogArgs] with SimpleRoutingApp with Spr
     }
   }
 
-  import geotrellis.spark.op.stats._
-  import geotrellis.spark.op.zonal.summary._
-  import geotrellis.raster.op.zonal.summary._
 
   // import scalaz._
 
@@ -170,88 +148,8 @@ object CatalogService extends ArgApp[CatalogArgs] with SimpleRoutingApp with Spr
   // }
 
 
-  val extents = Map(
-    "philadelphia" -> Extent(-75.9284841594,39.5076933259,-74.3543298352,40.5691292481),
-    "orlando"      -> Extent(-81.6640927758,28.2876159612,-81.0875104164,28.7973683779),
-    "sanjose"      -> Extent(-122.204281443,37.1344012781,-121.5923878102,37.5401705236),
-    "portland"     -> Extent(-123.015276532,45.2704007159,-122.3044835961,45.7602907701),
-    "usa"          -> Extent(-124.9268976258,25.3021853935,-65.521646682,49.0009765951),
-    "pa"           -> Extent(-80.8936945008,39.7560786402,-74.662271682,42.2519392104),
-    "world"        -> Extent(-176.8565908405,-80.2714428203,176.8357981709,83.5687714812)
-  )
 
-  def statsReponse(model: String, data: Seq[(DateTime, Double)]) =  
-    JsArray(JsObject(
-      "model" -> JsString(model),
-      "data" -> JsArray(
-        data.map { case (date, value) =>
-          JsObject(
-            "time" -> JsString(date.toString), 
-            "value" -> JsObject(
-              "mean" -> JsNumber(value),
-              "min" -> JsNumber(value * (1 + scala.util.Random.nextDouble/3)), // I know, I'm a liar
-              "max" -> JsNumber(value * (1 - scala.util.Random.nextDouble/3))
-            )
-          )
-        }: _*)
-    ))
-
-  def statsRoute = cors {
-    (pathPrefix(Segment / IntNumber) & get ) { (name, zoom) =>      
-      import DefaultJsonProtocol._         
-      
-      val layer = LayerId(name, zoom)
-      val (lmd, params) = catalog.metaDataCatalog.load(layer).get
-      println("LOADED", lmd, params)
-      val md = lmd.rasterMetaData  
-
-      parameters('city) { city => 
-        val extent = extents(city).reproject(LatLng, md.crs)
-        val bounds = md.mapTransform(extent)
-
-        path("multimodel") {
-          complete {
-            val model1 = catalog.load[SpaceTimeKey](LayerId("tas-miroc5-rcp45",4), FilterSet(SpaceFilter[SpaceTimeKey](bounds))).get
-            val model2 = catalog.load[SpaceTimeKey](LayerId("tas-access1-rcp45",4), FilterSet(SpaceFilter[SpaceTimeKey](bounds))).get
-            val model3 = catalog.load[SpaceTimeKey](LayerId("tas-cm-rcp45",4), FilterSet(SpaceFilter[SpaceTimeKey](bounds))).get
-            val ret = new RasterRDD[SpaceTimeKey](model1.union(model2).union(model3), md)
-              .mapKeys { key => key.updateTemporalComponent(key.temporalKey.time.withMonthOfYear(1).withDayOfMonth(1).withHourOfDay(0)) }
-              .averageByKey
-              .zonalSummaryByKey(extent, Double.MinValue, MaxDouble, stk => stk.temporalComponent.time)
-              .collect
-              .sortBy(_._1)
-
-            statsReponse("miroc5-access1-cm", ret)
-          }
-        } ~
-        path("max") { 
-          complete {    
-            statsReponse(name,
-              catalog.load[SpaceTimeKey](layer, FilterSet(SpaceFilter[SpaceTimeKey](bounds)))
-              .get
-              .mapKeys { key => key.updateTemporalComponent(key.temporalKey.time.withMonthOfYear(1).withDayOfMonth(1).withHourOfDay(0)) }
-              .averageByKey
-              .zonalSummaryByKey(extent, Double.MinValue, MaxDouble, stk => stk.temporalComponent.time)
-              .collect
-              .sortBy(_._1) )
-          } 
-        } ~      
-        path("min") { 
-          complete {    
-            statsReponse(name,
-              catalog.load[SpaceTimeKey](layer, FilterSet(SpaceFilter[SpaceTimeKey](bounds)))
-              .get
-              .mapKeys { key => key.updateTemporalComponent(key.temporalKey.time.withMonthOfYear(1).withDayOfMonth(1).withHourOfDay(0)) }
-              .averageByKey
-              .zonalSummaryByKey(extent, Double.MaxValue, MinDouble, stk => stk.temporalComponent.time)
-              .collect
-              .sortBy(_._1) )
-          } 
-        }    
-      }
-    }
-  }
-
+  
   def timedCreate[T](startMsg:String,endMsg:String)(f:() => T):T = {
     println(startMsg)
     val s = System.currentTimeMillis
@@ -323,7 +221,7 @@ object CatalogService extends ArgApp[CatalogArgs] with SimpleRoutingApp with Spr
   def root = {
     pathPrefix("catalog") { catalogRoute } ~
       pathPrefix("tms") { tmsRoute } ~
-      pathPrefix("stats") { statsRoute } ~
+      pathPrefix("stats") { zonalRoutes(catalog) } ~
       pixelRoute
   }
 

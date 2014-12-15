6e780c8267680a48343a96b21f2e2b62cd5f74ef
Eugene Cheipesh <echeipesh@gmail.com>
2014-12-15 13:16:57 -0500

    Allow methods to inspect CORS support

--- spark/src/main/scala/geotrellis/climate/rest/CorsSupport.scala
+++ spark/src/main/scala/geotrellis/climate/rest/CorsSupport.scala
@@ -9,7 +9,7 @@ import spray.routing._
 trait CorsSupport { self: HttpService =>
   val corsHeaders = List(`Access-Control-Allow-Origin`(AllOrigins),
     `Access-Control-Allow-Methods`(GET, POST, OPTIONS, DELETE),
-    `Access-Control-Allow-Headers`("Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding, Accept-Language, Host, Referer, User-Agent"))
+    `Access-Control-Allow-Headers`("Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding, Accept-Language, Host, Referer, User-Agent, Access-Control-Request-Method, Access-Control-Request-Headers"))
 
   def cors: Directive0 = {
     val rh = implicitly[RejectionHandler]

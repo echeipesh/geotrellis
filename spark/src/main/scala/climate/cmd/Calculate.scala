621e24b2e6afa2f164b2e0af4591635c8972da00
Eugene Cheipesh <echeipesh@gmail.com>
2015-03-16 15:39:05 -0400

    load specific extent

--- spark/src/main/scala/climate/cmd/Calculate.scala
+++ spark/src/main/scala/climate/cmd/Calculate.scala
@@ -30,7 +30,7 @@ object Calculate extends ArgMain[BenchmarkArgs] with Logging {
     val catalog = accumulo.catalog
     val layer = args.getLayers.head
     println(s"Working with: $layer")
-    val rdd = catalog.load[SpaceTimeKey](layer).cache
+    val rdd = Benchmark.getRdd(catalog, layer, Extents.extents("USA"), "USA")
     
     println(s"Record Count: ${rdd.count}")
     val things = rdd.mapPartitionsWithIndex( (idx, iter) => {
